0f518e69cf8262bb5f405ba87c05e95c18b0417e
Carlos Mijangos <manchester-714@hotmail.com>
2020-11-19 16:01:20 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+         static  void Main(string[] args)
+        {
+             valor();
+            Console.WriteLine("Hello World!");
+        }
+
+        public  async static Task valor() 
+        {
+            Task<int> myTask = Task.FromResult(1000);
+
+            // Podemos consultar el estado
+            // Los posibles valores están definidos en el enum TaskStatus
+            var taskStatus = myTask.Status;
+
+            // Existen propiedades booleanas para controlar los estados
+            var isTaskFaulted = myTask.IsFaulted;
+            var isTaskCanceled = myTask.IsCanceled;
+            var isTaskCompleted = myTask.IsCompleted;
+
+            // Esperaremos a que terminen las tareas usando await
+            var awaitedResult = await myTask;
+
+            // Nunca en código asíncrono lo haremos de esta forma
+            myTask.Wait();
+            var result = myTask.Result;
+        }
+    }
+}
